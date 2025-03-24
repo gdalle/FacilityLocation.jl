0f05f0faaa009aaa49bcfe868244e0cb17d74e4b
BatyLeo <leo.baty67@gmail.com>
2025-03-24 15:46:35 +0100

    cleanup script

--- scripts/main.jl
+++ scripts/main.jl
@@ -7,7 +7,7 @@ cpu = CPU()
 gpu = CUDA.CUDABackend()
 
 distance_cost = 0.1
-I, J, K = 20, 500, 250
+I, J, K = 20, 500, 400
 cpu_problem = FacilityLocationProblem(I, J, K; backend=cpu, distance_cost)
 gpu_problem = FacilityLocationProblem(I, J, K; backend=gpu, distance_cost)
 
